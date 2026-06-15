fd3e25115dc39ebd24487603c676a89c7e670349
Lanh Hoang <lanhhoang@users.noreply.github.com>
2026-06-14 23:47:52 -0400

    chore: scaffold pi-dcp extension package

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+
+export default function createExtension(pi: ExtensionAPI): void {
+  // Foundation handlers will be added in Task 7
+}
